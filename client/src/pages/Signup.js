f1bce773b01b825e30ca045d094b956a2a11f957
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2024-10-16 18:18:27 +0530

    Update Signup.js

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-const API_URL = `http://localhost:3001`;
+const API_URL = `https://socialmediaapp-cxsr.onrender.com`;
 
 const Signup = () => {
     const [name, setName] = useState('');
