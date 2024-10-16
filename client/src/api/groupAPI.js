40d6ac7016710f3c7102314c812ec70e7a92d761
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2024-10-16 18:14:10 +0530

    Update groupAPI.js

--- client/src/api/groupAPI.js
+++ client/src/api/groupAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api/groups';
+const API_URL = 'https://socialmediaapp-cxsr.onrender.com/api/groups';
 
 
 const getToken = () => localStorage.getItem('token'); // Adjust this based on where you're storing the token
